cd66bed053f5d26e3aa0bbf4efbe239a6969f107
rururux <rururux@gmail.com>
2025-11-03 07:58:59 +0900

    Icon コンポーネント実装

---
+++ app/components/Icon/index.tsx
@@ -0,0 +1,15 @@
+import type { HTMLAttributes } from "react"
+import styles from "./styles.module.css"
+import { mergeProps } from "react-aria"
+
+type IconProps = Omit<HTMLAttributes<SVGElement>, "children"> & { icon: "arrow-back" }
+
+export function Icon({ icon, ...props }: IconProps) {
+  const mergedProps = mergeProps<HTMLAttributes<SVGElement>[]>({ className: styles.icon }, props)
+
+  return (
+    <svg {...mergedProps}>
+      <use href={`/svg/icons.svg#${icon}`} />
+    </svg>
+  )
+}
