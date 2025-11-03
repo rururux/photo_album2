c2afb371c2abe2630d888ed4ef470e798821e627
rururux <rururux@gmail.com>
2025-11-03 17:10:00 +0900

    Button コンポーネント実装

---
+++ app/components/Button/index.tsx
@@ -0,0 +1,21 @@
+import type { ButtonHTMLAttributes } from "react"
+import { mergeProps } from "react-aria"
+import styles from "./styles.module.css"
+import capitalize from "~/utils/capitalize"
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  size?: "small",
+  variant?: "text"
+}
+
+export function Button({ size = "small", variant = "text", children, ...props }: ButtonProps) {
+  const classNames = [ styles.button, styles[`button${capitalize(size)}`], styles[`button${capitalize(variant)}`] ].join(" ")
+  const mergedProps = mergeProps({ className: classNames, type: "button" }, props)
+
+  return (
+    <button {...mergedProps}>
+      <div className={styles.buttonStateLayer} />
+      <div className={styles.buttonContentLayer}>{children}</div>
+    </button>
+  )
+}
