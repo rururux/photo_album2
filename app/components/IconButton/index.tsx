73e0e8fa2c8a7c1f290960364f0b196b6dafcbf2
rururux <rururux@gmail.com>
2025-11-11 15:53:05 +0900

    IconButton の Props 修正

--- app/components/IconButton/index.tsx
+++ app/components/IconButton/index.tsx
@@ -1,9 +1,9 @@
-import type { HTMLAttributes } from "react"
+import type { ButtonHTMLAttributes } from "react"
 import { mergeProps } from "react-aria"
 import styles from "./styles.module.css"
 import capitalize from "~/utils/capitalize"
 
-type IconButtonProps = HTMLAttributes<HTMLButtonElement> & {
+type IconButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   size?: "small" | "medium",
   variant?: "standard" | "outlined"
 }
@@ -14,7 +14,7 @@ export function IconButton({ size = "medium", variant = "standard", children, ..
     styles[`iconButton${capitalize(size)}`],
     styles[`iconButton${capitalize(variant)}`]
   ].join(" ")
-  const mergedProps = mergeProps<HTMLAttributes<HTMLButtonElement>[]>({ className: classNames }, props)
+  const mergedProps = mergeProps<ButtonHTMLAttributes<HTMLButtonElement>[]>({ className: classNames, type: "button" }, props)
 
   return (
     <button {...mergedProps}>
