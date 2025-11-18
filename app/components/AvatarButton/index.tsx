3e2a0060a92ddd12a89e15ee3465b80276589108
rururux <rururux@gmail.com>
2025-11-18 18:39:27 +0900

    型の改善

--- app/components/AvatarButton/index.tsx
+++ app/components/AvatarButton/index.tsx
@@ -1,11 +1,9 @@
 import { mergeProps } from "react-aria"
 import styles from "./styles.module.css"
-import type { HTMLAttributes } from "react"
+import type { ButtonHTMLAttributes } from "react"
 
-type AvatarButtonProps = HTMLAttributes<HTMLButtonElement>
-
-export function AvatarButton({ children, ...props }: HTMLAttributes<HTMLButtonElement>) {
-  const mergedProps = mergeProps<HTMLAttributes<HTMLButtonElement>[]>({ className: styles.avatarButton }, props)
+export function AvatarButton({ children, ...props }: ButtonHTMLAttributes<HTMLButtonElement>) {
+  const mergedProps = mergeProps<ButtonHTMLAttributes<HTMLButtonElement>[]>({ className: styles.avatarButton }, props)
 
   return (
     <button type="button" {...mergedProps}>
