da669aff4c9adc881036a2f0e746a5b02c933a07
rururux <rururux@gmail.com>
2025-11-18 20:13:45 +0900

    retry

--- workers/lib/auth.ts
+++ workers/lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth"
 import { drizzleAdapter } from "better-auth/adapters/drizzle"
 import type { SocialProviders } from "better-auth/social-providers"
 import { drizzle, DrizzleD1Database } from "drizzle-orm/d1"
+import { scryptSync } from "node:crypto"
 
 function getSocialProviders(options?: { clientSecret: string }) {
   return {
@@ -29,7 +30,50 @@ export const auth = betterAuth({
 
 export function initAuth(db: DrizzleD1Database, oauthClientSecret: string) {
   return betterAuth({
-    emailAndPassword: { enabled: true },
+    emailAndPassword: {
+      enabled: true,
+      // CPU Timelimit 対策
+      // https://github.com/better-auth/better-auth/issues/969#issuecomment-3024999773
+      password: {
+        hash: async (password) => {
+          const salt = crypto.getRandomValues(new Uint8Array(16));
+          const saltHex = Array.from(salt).map(b => b.toString(16).padStart(2, '0')).join('');
+
+          const key = scryptSync(
+            password.normalize("NFKC"),
+            saltHex,
+            64,
+            {
+              N: 16384,
+              r: 16,
+              p: 1,
+              maxmem: 128 * 16384 * 16 * 2
+            }
+          );
+
+          const keyHex = Array.from(key).map(b => b.toString(16).padStart(2, '0')).join('');
+          return `${saltHex}:${keyHex}`;
+        },
+        verify: async ({ hash, password }) => {
+          const [saltHex, keyHex] = hash.split(":");
+
+          const targetKey = scryptSync(
+            password.normalize("NFKC"),
+            saltHex,
+            64,
+            {
+              N: 16384,
+              r: 16,
+              p: 1,
+              maxmem: 128 * 16384 * 16 * 2
+            }
+          );
+
+          const targetKeyHex = Array.from(targetKey).map(b => b.toString(16).padStart(2, '0')).join('');
+          return targetKeyHex === keyHex;
+        },
+      }
+    },
     database: drizzleAdapter(db, { provider: "sqlite" }),
     socialProviders: getSocialProviders({ clientSecret: oauthClientSecret }),
     user: {
