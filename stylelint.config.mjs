c593951f673daba9f09a720708bab16f066e2d02
rururux <rururux@gmail.com>
2025-11-19 00:55:21 +0900

    stylelint 導入・適用

---
+++ stylelint.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import("stylelint").Config} */
+export default {
+  extends: [ "stylelint-config-standard", "stylelint-config-recess-order" ],
+  rules: {
+    "selector-class-pattern": null,
+    "comment-empty-line-before": null,
+    "custom-property-empty-line-before": null
+  }
+}
