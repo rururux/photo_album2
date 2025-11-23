6487bb0d84bdef1746211f322974b4c8090d7581
rururux <rururux@gmail.com>
2025-11-24 01:46:49 +0900

    install `eslint-plugin-import` and others

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,21 +3,38 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
+import importPlugin from "eslint-plugin-import"
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     plugins: { js },
-    extends: ["js/recommended" ],
+    extends: [ "js/recommended" ],
     languageOptions: { globals: {...globals.browser, ...globals.node} }
   },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   pluginReact.configs.flat["jsx-runtime"],
   {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript
+    ],
+    rules: {
+      "import/order": "warn"
+    }
+  },
+  {
     settings: {
       react: {
         version: "19.1.1"
+      },
+      "import/resolver": {
+        typescript: {
+          project: "./tsconfig.*.json"
+        },
+        node: true
       }
     }
   }
